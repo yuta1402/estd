39e9a301e3af3c19f34c130fa3092ccddf78d170
gut <yuta1402souja@live.jp>
2019-10-03 13:34:03 +0900

    Add simple test about ndvector

--- test/ndvector/ndvector_test.hpp
+++ test/ndvector/ndvector_test.hpp
@@ -1,9 +1,27 @@
+#include "test.hpp"
 #include "ndvector.hpp"
 
 void ndvector_test()
 {
-    estd::ndvector<int> v(5, 5, 4, 2);
-    v(1, 1, 1, 1) = 100;
-    std::cout << v(1, 1, 1, 1) << std::endl;
-    std::cout << v(0, 1, 1, 1) << std::endl;
+    estd::ndvector<int> v(2, 2, 3);
+
+    int c = 0;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                v(i, j, k) = c;
+                ++c;
+            }
+        }
+    }
+
+    c = 0;
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                eassert(v(i, j, k) == c, "v(i, j, k) == %d, c == %d", v(i, j, k), c);
+                ++c;
+            }
+        }
+    }
 }
